f70280d2a0c85e4cd18b4e9023f8885558fb0cc1
Jan Rocho <jan.rocho@fh-dortmund.de>
2017-11-16 12:23:19 +0100

    ILIAS 5.3

--- plugin.php
+++ plugin.php
@@ -4,12 +4,12 @@
 $id = "xpdl";
  
 // code version; must be changed for all code changes
-$version = "1.3.1";
+$version = "1.3.2";
  
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
 $ilias_min_version = "4.3.0";
-$ilias_max_version = "5.2.999"; 
+$ilias_max_version = "5.3.999"; 
  
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = "Jan Rocho";
