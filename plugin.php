cb56d37ed9612c30091fb9138c6bfaf5746ca25b
Jan Rocho <jan.rocho@fh-dortmund.de>
2020-10-30 09:39:07 +0100

    preparing for ilias6

--- plugin.php
+++ plugin.php
@@ -9,7 +9,7 @@ $version = "1.4.3";
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
 $ilias_min_version = "5.3.0";
-$ilias_max_version = "5.4.999"; 
+$ilias_max_version = "6.999"; 
  
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = "Jan Rocho";
